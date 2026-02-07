3fe3ca0730d43429bfed25ca809527f9ba8a668b
Vu Nguyen <nguyenlongvuvlog.com>
2026-02-07 12:11:12 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { React } from 'react'
+import Header from './components/layouts/Header'
+import Footer from './components/layouts/Footer'
+import HomePage from './pages/Home'
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <HomePage />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/home/HomeBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomeBanner() {
+  return (
+    <div>
+      <h1>Home Banner</h1>
+    </div>
+  )
+}
+
+export default HomeBanner
